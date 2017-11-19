cbb77a3de9a0c75151281f4b7e03a70f86037302
kurenaif <antyobido@gmail.com>
2017-11-19 17:02:17 +0900

    [add] parse_Score

--- evaluate.sh
+++ evaluate.sh
@@ -2,15 +2,19 @@
 
 make
 
+echo `date` > files/score.out
+
 for i in `seq -w 1 20`
 do
     ./graph_generator.out files/random_${i}.in 0 $i
     ./hokudai_procon1 < files/random_${i}.in > files/random_${i}.out
-    ./score_evaluator.out files/random_${i}.in files/random_${i}.out
+    echo random_${i} >> files/score.out
+    ./score_evaluator.out files/random_${i}.in files/random_${i}.out >> files/score.out
 done
 for i in `seq -w 1 20`
 do
     ./graph_generator.out files/complete_${i}.in 1 $i
     ./hokudai_procon1 < files/complete_${i}.in > files/complete_${i}.out
-    ./score_evaluator.out files/complete_${i}.in files/complete_${i}.out
+    echo complete_${i} >> files/score.out
+    ./score_evaluator.out files/complete_${i}.in files/complete_${i}.out >> files/score.out
 done
---
+++ parse_score.py
@@ -0,0 +1,14 @@
+f = open('files/score.out')
+date = f.readline()
+
+i = 0
+score = 0
+for line in f.readlines():
+    if i % 2 == 0:
+        i += 1
+        continue
+    score += int(line.split(':')[1].split('/')[0])
+    i += 1
+
+print(date[:-1])
+print('score:', score)
