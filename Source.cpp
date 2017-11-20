3c2798a44f3347faab6933ec609bbf3b42599ccf
kurenaif <antyobido@gmail.com>
2017-11-20 14:42:34 +0900

    DFS & swap

--- Source.cpp
+++ Source.cpp
@@ -4,21 +4,23 @@
 #include <ciso646>
 #include <algorithm>
 #include <cmath>
-#include <queue>
-#include <unordered_set>
 #include <climits>
-#include <chrono>
 #include <random>
- 
- 
+#include <set>
+#include <queue>
+#include <map>
+#include <chrono>
+
+
 #define FOR(i,a,b) for (int i=(a);i<(b);i++)
 #define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
 #define REP(i,n) for (int i=0;i<(n);i++)
 #define RREP(i,n) for (int i=(n)-1;i>=0;i--)
- 
- 
+
+
 using namespace std;
 
+constexpr double limit = 9500;
 
 
 class xor128 {
@@ -46,6 +48,7 @@ public:
 private:
 	unsigned x = 123456789u, y = 362436069u, z = 521288629u, w;
 };
+
 
 class Graph {
 public:
@@ -81,7 +84,7 @@ public:
 		return mat[from][to];
 	}
 };
- 
+
 class Check {
 public:
 	vector<int> checkArray;
@@ -98,7 +101,7 @@ public:
 	vector<int> get_not_checks() const {
 		vector<int> res;
 		REP(i, isChecked.size()) if (not isChecked[i]) { res.push_back(i); }
-		return std::move(res);
+		return res;
 	}
 };
 bool operator < (const Check& left, const Check& right){
@@ -116,16 +119,114 @@ struct CheckHash {
 		return std::hash<vector<bool>>()(key.isChecked);
 	};
 };
- 
+
+
 void map_graph(Check& envCheck, int envNode, unordered_map<int, int>& phi, Check& gCheck, int gNode) {
 	phi[envNode] = gNode;
 	envCheck.check(envNode);
 	gCheck.check(gNode);
 }
- 
+
+struct State{
+	unordered_map<int, int> phi;
+	Check envCheck;
+	Check gCheck;
+	int score;
+	State(const unordered_map<int, int>& phi, const Check& envCheck, const Check& gCheck, const int score):phi(phi), envCheck(envCheck), gCheck(gCheck), score(score){}
+	State(const unordered_map<int, int>& phi, const Check& envCheck, const Check& gCheck):phi(phi), envCheck(envCheck), gCheck(gCheck), score(0){}
+};
+
+struct StateHash {
+	typedef std::size_t result_type;
+	std::size_t operator()(const State& key) const{
+		return CheckHash()(key.gCheck) ^ CheckHash()(key.envCheck);
+	};
+};
+bool operator < (const State& left, const State& right){
+    if(left.score != right.score) return left.score < right.score;
+	if(left.envCheck != right.envCheck) return left.envCheck < right.envCheck;
+    return left.gCheck < right.gCheck;
+}
+bool operator == (const State& left, const State& right){
+	return left.gCheck == right.gCheck and left.envCheck == right.envCheck;
+}
+unordered_map<State, vector<State>, StateHash> stateMemo;// state, size: value vector<State>
+vector<State> score(const Graph& G, const Graph& envG, const State& state, int size){
+	if(stateMemo.find(state) != stateMemo.end()) {
+		return stateMemo[state];
+	}
+	const unordered_map<int, int>& phi = state.phi;
+	const Check& envCheck = state.envCheck;
+	const Check& gCheck = state.gCheck;
+    if(phi.size() == G.size()) return {};
+    vector<State> res(size, state);
+	unordered_map<int, vector<int>> envNodes;
+	for (auto &n : envCheck.get_checked_nodes()) for (auto &t : envG.get_to(n)) if (not envCheck.get_is_checked(t)) {
+				envNodes[t].push_back(n);
+			}
+	unordered_map<int, pair<int, double>> envScores; // env側でのnodeの評価値 envScores[envTo] = map(gTo, score);
+	for (const pair<int, vector<int> > &nodePair : envNodes) { // ↑の処理でやったenv側のノード first:to, second: froms
+		unordered_map<int, double> gScores; // scores[to] = score;
+		//G側でスコアの精算
+		set<int> gCheckNodes; // g側でcheckしたnode
+		vector<int> notChecked = gCheck.get_not_checks();
+		for (const int &envCheckedNode : nodePair.second) {
+			int from = phi.at(envCheckedNode);// envG -> G
+			for (int to : notChecked) { // unchecked G nodes
+				gScores[to] += G.get_cost(from, to);
+				gCheckNodes.insert(to);
+			}
+		}
+		//G側で最大値
+		pair<int, double> best = *std::max_element( //first: to, second: score
+				gScores.begin(), gScores.end(),
+				[](const pair<int, int>& left, const pair<int, int>& right) {
+					return left.second < right.second;
+				}
+		);
+		envScores[nodePair.first] = best;
+	}
+	//env側で最大値
+	pair<int, pair<int, double>> best = *std::max_element( //first: envTo, second: (first: gTo, score)
+			envScores.begin(), envScores.end(),
+			[](const pair<int, pair<int, int>>& left, const pair<int, pair<int, int>>& right) {
+				return left.second.second < right.second.second;
+			}
+	);
+	vector<pair<double, pair<int, int> > > bestNodes;
+	for(const auto &a:envScores){
+		bestNodes.push_back({a.second.second, {a.first, a.second.first}});
+	}
+	sort(bestNodes.begin(), bestNodes.end());
+	reverse(bestNodes.begin(), bestNodes.end());
+	//map graph
+    REP(i,res.size()){
+        State& s = res[i];
+		map_graph(s.envCheck, bestNodes[i].second.first, s.phi, s.gCheck, bestNodes[i].second.second);
+        s.score = bestNodes[i].first;
+	}
+	stateMemo[state] = res;
+	return std::move(res);
+}
+
+int DFS(const Graph& G, const Graph& envG, const State& state, int width, int depth){
+	if(depth == 0 or state.phi.size() == G.size()) return state.score;
+	vector<State> next = score(G, envG, state, width);
+	int ma = state.score;
+	REP(i,next.size()){
+		ma = max(DFS(G, envG, next[i], width, depth-1), ma);
+	}
+	return ma;
+}
+
+
 int main(void) {
 	chrono::system_clock::time_point  start, end; // 型は auto で可
 	start = std::chrono::system_clock::now(); // 計測開始時間
+	// fast cin
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	xor128 random;
 	// input
 	int V, E; cin >> V >> E;
 	Graph G(V); //0-indexed node
@@ -143,66 +244,50 @@ int main(void) {
 		--u; --v;
 		envG.add_edge(u, v, -1);
 	}
- 
+
 	unordered_map<int, int> phi;
 	// first node
-	pair<int, int> bestFirst; // first:score, second:node 
+	pair<int, int> bestFirst; // first:score, second:node
 	REP(from, G.size()) {
-		int score = 0;
-		for (auto &to : G.get_to(from)) {
-			score += G.get_cost(from, to);
-		}
+		int score = G.get_edge_sum(from);
 		if (bestFirst.first < score) {
 			bestFirst.first = score;
 			bestFirst.second = from;
 		}
 	}
 	int center_node = envG.size() / 2;
-	if (envG.size() % 2 == 0) center_node -= sqrt(envG.size())/2;
-	phi[center_node] = bestFirst.second;
+	if (envG.size() % 2 == 0) center_node -= sqrt(envG.size()) / 2;
 	map_graph(envCheck, center_node, phi, gCheck, bestFirst.second);
- 
- 
-	REP(i, G.size() - 1) {
-		unordered_map<int, vector<int>> envNodes;
-		for (auto &n : envCheck.get_checked_nodes()) for (auto &t : envG.get_to(n)) if (not envCheck.get_is_checked(t)) {
-			envNodes[t].push_back(n);
+
+    queue<State> que;
+	que.push(State(phi, envCheck, gCheck, 0));
+
+	vector<State> lasts;
+    while(not que.empty()) {
+		State state = que.front(); que.pop();
+		if(state.phi.size() == G.size()){
+			lasts.push_back(state);
+            continue;
 		}
-		unordered_map<int, pair<int, int>> envScores; // env側でのnodeの評価値 envScores[envTo] = map(gTo, score);
-		for (const pair<int, vector<int> > &nodePair : envNodes) { // ↑の処理でやったenv側のノード first:to, second: froms
-			unordered_map<int, int> gScores; // scores[to] = score;
-			//G側でスコアの精算
-			//envCheckedNodeでメモ化可能
-			for (const int &envCheckedNode : nodePair.second) {
-				int from = phi[envCheckedNode];// envG -> G
-				for (int to : gCheck.get_not_checks()) { // unchecked G nodes
-					gScores[to] += G.get_cost(from, to);
-				}
-			}
-			//G側で最大値
-			pair<int, int> best = *std::max_element( //first: to, second: score
-				gScores.begin(), gScores.end(),
-				[](const pair<int, int>& left, const pair<int, int>& right) {
-				return left.second < right.second;
-			}
-			);
-			envScores[nodePair.first] = best;
+        vector<State> next = score(G, envG, state, 4);
+		vector<int> points(next.size());
+        REP(i,next.size()){
+			points[i] = DFS(G, envG, next[i], 3, 1);
 		}
-		//env側で最大値
-		pair<int, pair<int, int>> best = *std::max_element( //first: to, second: score
-			envScores.begin(), envScores.end(),
-			[](const pair<int, pair<int, int>>& left, const pair<int, pair<int, int>>& right) {
-			return left.second.second < right.second.second;
-		}
-		);
-		map_graph(envCheck, best.first, phi, gCheck, best.second.first);
+        int index = max_element(points.begin(), points.end()) - points.begin();
+        que.push(next[index]);
 	}
+	State res = *max_element(lasts.begin(), lasts.end());
 
 	priority_queue<pair<int, int> > nodePotential; // first: score, second: envNode
 	vector<int> scoreMemo(envG.size()); // scoreMemo[to] = score;
-
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
 	//各ノードが増やせる可能性がある量を列挙
 	for(pair<int, int> nodeMap: phi){
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
+		if(elapsed > limit) break;
 		int envFrom = nodeMap.first;
 		int gFrom = phi[envFrom];
 		int score = G.get_edge_sum(gFrom);
@@ -216,7 +301,6 @@ int main(void) {
 		nodePotential.push({score, envFrom});
 	}
 
-	double limit = 9500;
 	while(true){
 		end = std::chrono::system_clock::now();  // 計測終了時間
 		double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
@@ -227,6 +311,9 @@ int main(void) {
 		int curPenalty = scoreMemo[curNode]; // cur側を取り除いた時に発生する負債
 		pair<int, int> maxScore; //first: score, second: tar
 		REP(tarNode,envG.size()){
+			end = std::chrono::system_clock::now();  // 計測終了時間
+			double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(); //処理に要した時間をミリ秒に変換
+			if(elapsed > limit) break;
 			int tarPenalty = scoreMemo[tarNode]; // tar側を取り除いた時に発生する負債
 			int score = 0;
 			for(int to:envG.get_to(tarNode)){ //入れ替えた先がtar側の計算
@@ -258,7 +345,7 @@ int main(void) {
 			else {
 				swap(phi[curNode], phi[maxScore.second]);
 			}
-
+ 
 			int envFrom = curNode;
 			if(phi.count(envFrom)){
 				int gFrom = phi[envFrom];
@@ -272,7 +359,7 @@ int main(void) {
 				}
 				nodePotential.push({score, envFrom});
 			}
-
+ 
 			envFrom = maxScore.second;
 			if(phi.count(envFrom)){
 				int gFrom = phi[envFrom];
@@ -288,8 +375,9 @@ int main(void) {
 			}
 		}
 	}
+	
 
-	for (auto &a : phi) {
-		cout << a.second+1 << " " << a.first+1 << endl;
+	for (auto &a : res.phi) {
+		cout << a.second + 1 << " " << a.first + 1 << endl;
 	}
 }
